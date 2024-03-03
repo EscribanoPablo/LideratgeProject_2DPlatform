9be297f07722d8a458a631e95ef10b3a8042a1ee
Alex Morillas <60931534+Skfod37@users.noreply.github.com>
2024-03-03 15:57:54 +0100

    cohete y plataforma rompible

---
+++ 2D_Platform_Lideratge/Assets/Scripts/CoheteLauncher.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoheteLauncher : MonoBehaviour
+{
+    [SerializeField] private GameObject _cohete;
+    [SerializeField] private Transform _shootPosition;
+
+    [SerializeField] private Vector2 _coheteSpeed;
+    [SerializeField] private float _shootTime;
+    [SerializeField] private float _startDelay;
+
+    private float _shootTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _shootTimer = Time.time + _shootTime + _startDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= _shootTimer)
+        {
+            _shootTimer = Time.time + _shootTime;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Rigidbody2D rb = Instantiate(_cohete, _shootPosition.position, Quaternion.identity).GetComponent<Rigidbody2D>();
+        rb.velocity = _coheteSpeed;
+        Destroy(rb.gameObject, 10.0f);
+    }
+}
